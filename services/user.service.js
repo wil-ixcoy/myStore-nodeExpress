4c95edc40c7a663ffcc0194d4768f3db18007e1d
Wiliams Alexander Tzoc Ixcoy <wiliamsg200295@gmail.com>
2022-03-26 11:29:07 -0600

    Se hace un crud con user.service.js

--- services/user.service.js
+++ services/user.service.js
@@ -1,4 +1,4 @@
-//const boom = require('@hapi/boom');
+const boom = require('@hapi/boom');
 //const getConnection  = require('../libs/postgres');
 
 //sequelize no tiene ni un models escrito, pero el ORM guarda con un nombre y por eso se llama a ese objeto
@@ -6,28 +6,35 @@ const {models} = require("../libs/sequelize");
 
 class UserService {
   constructor() {}
-
+//funcion para crear un usuario, recibe un objeto con los datos del usuario
   async create(data) {
-    return data;
+    const newUSer = await models.User.create(data);
+    return newUSer;
   }
   //usamos el modelo con nombre User que guardo sequelize y traemos todo lo que tenga y lo retornamos
   async find() {
     const resultado = await models.User.findAll();
     return resultado;
   }
-
+//funcion para buscar un usuario por su id usamos findByPk para buscar por id luego comparamos que sea
+//distinto de nulo si lo es retornamos un error con boom
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if (!user) {
+      throw boom.notFound("User not found");
+    }
+    return user;
   }
-
+//usamos la funcion findOne para buscar un usuario por su id y luego usamos update para actualizar
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const resultado = user.update(changes);
+    return resultado;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
